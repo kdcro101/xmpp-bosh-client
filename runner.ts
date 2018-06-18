3aa27cd6f1bd6dd4466b64b01104108e66bc65a5
kdcro101 <kdcro101.github@github.com>
2018-06-18 17:20:21 +0200

    tests

--- runner.ts
+++
@@ -1,8 +0,0 @@
-const Jasmine = require('jasmine')
-const jasmineInstance = new Jasmine()
-
-jasmineInstance.loadConfigFile('spec/support/jasmine.json')
-jasmineInstance.configureDefaultReporter({
-    showColors: true
-})
-jasmineInstance.execute();
